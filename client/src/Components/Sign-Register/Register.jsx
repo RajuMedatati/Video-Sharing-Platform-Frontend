450c64b40e0a289693795f58dd2d26f079a2743a
Rajesh mishra <rkm141297@gmail.com>
2023-03-25 15:08:40 +0530

    register and signin page done

--- client/src/Components/Sign-Register/Register.jsx
+++ client/src/Components/Sign-Register/Register.jsx
@@ -1,8 +1,74 @@
 import "./Style.css"
+import React,{useState }from "react";
+
+
+import { useNavigate } from 'react-router-dom';
+
 const Register=()=>{
+    const navigate = useNavigate();
+
+    const [user,setUser]=useState({name:"",email:"",phone:"",profession:"",password:"",cpassword:""});
+
+      let name,value;
+      const handleInputs=(e)=>{
+        // console.log(e)
+        name=e.target.name;
+        value=e.target.value;
+        setUser({...user,[name]:value})
+        console.log(user)
+
+
+    }
+
+    const Postdata=async(e)=>{
+        e.preventDefault();
+        const {name,email,phone,profession,password,cpassword}=user;
+        const res= await fetch("/register",{
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify({name,email,phone,profession,password,cpassword})
+        });
+        const data=await res.json()
+        if(data.status==422 || !data){
+            window.alert("INVALID Registration")
+            console.log("INVALID Registration")
+        }
+        else{
+            window.alert(" Registration Succesfull")
+            console.log("Registration succesfull")
+            // history.push("/signin")
+        }
+      
+    }
     return(
         <>
-        I am from register
+       <main>
+            <section className="sec-first">
+            <img src="https://img.buzzfeed.com/buzzfeed-static/static/2022-05/6/11/campaign_images/9615c13b38f0/the-47-best-mother-son-movies-to-watch-on-mothers-2-1448-1651836261-30_dblbig.jpg?resize=1200:*" alt=""/>
+                <div className='logo'>
+                <h1 >Tuner</h1>
+                <div >Enjoy Multiple videos at one place</div>
+                
+                </div>
+                <button  onClick={() => {navigate("/sign")}}>Signin</button>
+            </section>
+            <section className="sec-second register">
+                <form method="POST" >
+                    <h1>Register</h1>
+                    <div>Register to continue access pages</div>
+                    <button id='btn-circle'>+</button><br />
+                    <input type="text" name="name"  id="name" placeholder="Name" value={user.name} onChange={handleInputs}/><br />
+                    <input type="email" name="email" id="email" placeholder="Email" value={user.email} onChange={handleInputs}/><br />
+                    <input type="number" name="phone" id="phone" placeholder="Phone" value={user.phone} onChange={handleInputs}/><br />
+                    <input type="text" name="profession" id="profession" placeholder="Profession" value={user.profession} onChange={handleInputs}/><br />
+                    <input type="password" name="password" id="password" placeholder="Password" value={user.password} onChange={handleInputs}/><br />
+                    <input type="password" name="cpassword" id="cpasswword" placeholder="Confirm Password" value={user.cpassword} onChange={handleInputs}/><br />
+                    <input  className="submit" type="submit" name="signup" id="signup" value="Register" onClick={Postdata}></input><br />
+                </form>
+            </section>
+        </main>
         </>
     )
 }
--- client/src/Components/Sign-Register/Signin.jsx
+++ client/src/Components/Sign-Register/Signin.jsx
@@ -1,9 +1,31 @@
-import "./Style.css"
-const Signin=()=>{
-    return(
-        <>
-        I am from Signin
-        </>
-    )
+import './Style.css'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+
+function SingIn(){
+    const navigate = useNavigate();
+    return (<>
+        <main>
+            <section className="sec-first">
+                <img src="https://img.buzzfeed.com/buzzfeed-static/static/2022-05/6/11/campaign_images/9615c13b38f0/the-47-best-mother-son-movies-to-watch-on-mothers-2-1448-1651836261-30_dblbig.jpg?resize=1200:*" alt=""/>
+                <div className='logo'>
+                <h1 >Tuner</h1>
+                <div >Enjoy Multiple videos at one place</div>
+                
+                </div>
+                <button  onClick={() => navigate('/register')}>Register</button>
+            </section>
+            <section className="sec-second">
+                <form action="#"  className='formdata'>
+                    <h1>Sing In</h1>
+                    <div>Sing in to continue access pages</div><br />
+                    <input type="email" name="email" id="email" placeholder="Email"/><br />
+                    <input type="password" name="passord" id="password" placeholder="Password"/><br />
+                    <button type="submit">Sing in</button>
+                </form>
+            </section>
+        </main>
+    </>);
 }
-export default Signin;
+export default SingIn;
