67caabf4163b4a840e08dc6ae1e6ab91f44cfd1f
Ranjit Bera <ranjitbera34567@gmail.com>
2023-03-29 20:07:16 +0530

    changes made on my videos section

--- client/src/Components/Main/Navbar/Card.jsx
+++ client/src/Components/Main/Navbar/Card.jsx
@@ -6,10 +6,10 @@ const Card = ({data})=> {
     const [toggle,setToggle] = useState(false)
   return (
     <>{!toggle?
-    <div className='card'>
+    <div className='card' onClick={()=>{setToggle(true)}}>
         <img src={data.imgUrl} alt='image not found'/>
         <div className='btn'>
-            <img src='https://www.freeiconspng.com/thumbs/video-play-icon/video-play-icon-24.png' alt='img not' onClick={()=>{setToggle(true)}}/>
+            <img src='https://www.freeiconspng.com/thumbs/video-play-icon/video-play-icon-24.png' alt='img not'/>
         </div>
         <h3>{data.tittle}</h3>
     </div>:
